3e2e1c02a0d118999c88125bbb2d4e0a1c44bc8b
Samuel Walker <ldspcjunkie@gmail.com>
2019-07-29 12:46:59 -0600

    some commit

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -18,7 +18,7 @@ public:
 		ImGui::Begin("Clear Color");
 		glm::vec4 col = app.renderer->getClearColor();
 		float* rgba = new float[3]{ col[0], col[1], col[2] };
-		if (ImGui::ColorPicker3("", rgba)) {
+		if (ImGui::ColorPicker3("Clear Color", rgba)) {
 			app.renderer->setClearColor(glm::vec4(rgba[0], rgba[1], rgba[2], 1.0));
 		}
 		ImGui::End();
