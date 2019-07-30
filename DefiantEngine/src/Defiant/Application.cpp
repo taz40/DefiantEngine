1954df81e7ba7e354300977f2323c40b5f2a3c47
Samuel Walker <ldspcjunkie@gmail.com>
2019-07-30 07:41:54 -0600

    buffer layout abstraction

--- DefiantEngine/src/Defiant/Application.cpp
+++ DefiantEngine/src/Defiant/Application.cpp
@@ -12,6 +12,24 @@ namespace Defiant {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type) {
+		case ShaderDataType::Float:	 return GL_FLOAT;
+		case ShaderDataType::Float2: return GL_FLOAT;
+		case ShaderDataType::Float3: return GL_FLOAT;
+		case ShaderDataType::Float4: return GL_FLOAT;
+		case ShaderDataType::Mat3:   return GL_FLOAT;
+		case ShaderDataType::Mat4:   return GL_FLOAT;
+		case ShaderDataType::Int:    return GL_INT;
+		case ShaderDataType::Int2:   return GL_INT;
+		case ShaderDataType::Int3:   return GL_INT;
+		case ShaderDataType::Int4:   return GL_INT;
+		case ShaderDataType::Bool:   return GL_BOOL;
+		}
+		DE_CORE_ASSERT(false, "Unknown Data Type");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		DE_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -26,16 +44,25 @@ namespace Defiant {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		BufferLayout layout = {
+			{ShaderDataType::Float3, "a_Position"},
+			{ShaderDataType::Float4, "a_Color"}
+		};
+		m_VertexBuffer->SetLayout(layout);
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		uint32_t index = 0;
+		for (const auto& element : m_VertexBuffer->GetLayout()) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, m_VertexBuffer->GetLayout().GetStride(), (const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[3] = {
 			0, 1, 2
@@ -47,10 +74,13 @@ namespace Defiant {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 			
 			void main(){
+				v_Color = a_Color;
 				v_Position = a_Position;
 				gl_Position = vec4(a_Position, 1.0);
 			}	
@@ -62,9 +92,11 @@ namespace Defiant {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 			
 			void main(){
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}	
 		)";
 
--- DefiantEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ DefiantEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -8,10 +8,15 @@ namespace Defiant {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
@@ -19,10 +24,10 @@ namespace Defiant {
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
-		virtual uint32_t GetCount() const { return m_Count; }
+		virtual uint32_t GetCount() const override { return m_Count; }
 
 	private:
 		uint32_t m_Count;
